5dd01b5d03957f7148df149233802ac2c3335613
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-08-16 00:46:00 +0700

    Config: Added mongoUser, password

--- backend/config/config.go
+++ backend/config/config.go
@@ -1,11 +1,20 @@
 package config
 
 const (
+	//Redis Config
 	RedisAddr    = "localhost:6379"
 	RedisPass	 = ""
 	RedisDB		 = 0
+
+	//MongoDB Config
 	MongoURI     = "mongodb://localhost:27017"
+	MongoUser	 = "admin"
+	MongoPass	 = "admin"
+
+	//Kafka Config
 	KafkaBroker  = "localhost:9092"
 	KafkaTopic   = "test_topic"
+
+	//Server Config
 	Port         = "8080"
 )
