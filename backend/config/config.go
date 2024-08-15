6e39457a24e20854871dc587d116d5b94bf014e5
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-08-15 22:35:40 +0700

    Config: Added Config

---
+++ backend/config/config.go
@@ -0,0 +1,11 @@
+package config
+
+const (
+	RedisAddr    = "localhost:6379"
+	RedisPass	 = ""
+	RedisDB		 = 0
+	MongoURI     = "mongodb://localhost:27017"
+	KafkaBroker  = "localhost:9092"
+	KafkaTopic   = "test_topic"
+	Port         = "8080"
+)
