dbfdc791c706b7a46b2789476582b483f82c2e3e
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-08-18 23:53:07 +0700

    Config: Remove kafka, etc.

--- backend/config/config.go
+++ backend/config/config.go
@@ -8,13 +8,4 @@ const (
 
 	//MongoDB Config
 	MongoURI     = "mongodb://localhost:27017/test"
-	// MongoUser	 = "admin"
-	// MongoPass	 = "RML40Dbr9sxHHlwG"
-
-	//Kafka Config
-	// KafkaBroker  = "localhost:9092"
-	// KafkaTopic   = "test_topic"
-
-	//Server Config
-	Port         = "8080"
 )
