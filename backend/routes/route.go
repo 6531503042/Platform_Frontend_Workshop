39229fe13b9a7c535880f5b0191715e5ad1096e3
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-08-18 15:51:09 +0700

    Route: Added product routes

--- backend/routes/route.go
+++ backend/routes/route.go
@@ -16,4 +16,11 @@ func Setup(app *fiber.App) {
 	app.Get("/user-count", controllers.GetUserCount)
 	app.Get("/user-statistics", controllers.GetUserStatistics)
 
+	app.Post("/products", controllers.CreateProduct)
+	app.Get("/products/:id", controllers.GetProduct)
+	app.Get("/products", controllers.ListProduct)
+	app.Put("/products/:id", controllers.UpdateProduct)
+	app.Delete("/products/:id", controllers.DeleteProduct)
+	app.Get("/product-count", controllers.GetProductCount)
+	app.Get("/product-statistics", controllers.GetProductStatistics)
 }
