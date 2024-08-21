17a37078d55f4b07bcd07dd2226f9805172195af
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-08-21 14:47:10 +0700

    Model: Fixed Data Integrity of Status from string -> bool

--- backend/models/order.go
+++ backend/models/order.go
@@ -7,5 +7,5 @@ type Order struct {
 	UserID primitive.ObjectID `json:"user_id" bson:"user_id"`
 	ProductID primitive.ObjectID `json:"product_id" bson:"product_id"`
 	Quantity int `json:"quantity" bson:"quantity"`
-	Status string `json:"status" bson:"status"`
+	Status bool `json:"status" bson:"status"`
 }
