a4c2cacbdb931eebe6db851584fc38c43cf3e07e
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-08-21 16:21:43 +0700

    Route: Remove unused line

--- backend/routes/route.go
+++ backend/routes/route.go
@@ -8,14 +8,10 @@ import (
 
 func Setup(app *fiber.App) {
 
-	
-
 	// Initialize controller
 	userController := controllers.NewUserController()
 	productController := controllers.NewProductController()
 	orderController := controllers.NewOrderController()
-	
-
 
 	//User
 	app.Post("/users", userController.CreateUser)
